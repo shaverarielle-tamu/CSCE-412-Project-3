ff2d7bcbb129388fe20ef67540de321010379992
Arielle <arielle@Arielles-Laptop.local>
2024-10-15 23:51:28 -0500

    Adding functions for randomly generating data

--- Requests.cpp
+++ Requests.cpp
@@ -1,1 +1,71 @@
+#include "Requests.h"
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <random>
 
+Requests::Requests(){
+    ip_in = generateIP();
+    ip_out = generateIP();
+    Time = generateTime();
+    job_type = generateJobType();
+}
+
+string Requests::generateIP(){
+    srand(static_cast <unsigned>(time(0)));
+    int it = 0;
+    string IP;
+    int count = 2;
+    while(it < 7){
+        if(count%2==0){
+            IP += std::to_string(rand()%256);
+        }
+        else {
+            IP += ".";
+        }
+        count+=1;
+        it+=1;
+    }
+    return IP; 
+}
+
+int Requests::generateTime(){
+    int time = rand() % 101;
+    return time; 
+}
+
+char Requests::generateJobType() {
+    int num;
+
+    num = rand() % 2;
+
+    if(num==0){
+        return 'P';
+    }
+    else {
+        return 'S';
+    }
+}
+
+string Requests::get_ip_in() {
+    return ip_in;
+}
+
+string Requests::get_ip_out() {
+    return ip_out;
+}
+
+char Requests::get_job_type(){
+    return job_type;
+}
+
+/* For testing 
+
+int main () {
+    Requests req;
+    std::cout << req.get_ip_in() << std::endl;
+    std::cout << req.get_job_type() << std::endl;
+}
+*/
--- Requests.h
+++ Requests.h
@@ -2,6 +2,8 @@
 #ifndef REQUESTS_H
 #define REQUESTS_H
 #include <string>
+#include <cstdlib>
+#include <ctime>
 #include <random>
 
 using std::string;
@@ -9,8 +11,13 @@ class Requests {
     private:
         string ip_in;
         string ip_out;
-        int time;
+        int Time;
         char job_type;
+
+        // Functions to generate data
+        int generateTime();
+        char generateJobType();
+        string generateIP();
     public:
         Requests();
         string get_ip_in();
